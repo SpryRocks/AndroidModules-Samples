c2a12bf9d980bb805d04d4ad2246e9ac4d8e7f22
maxim <maxim.zhemerenko@spryrocks.com>
2017-05-15 20:02:24 +0300

    Init project

---
+++ Sources/ImageSearch/presentation/src/main/java/com/spryrocks/imgsearch/presentation/ui/screens/MainActivity.java
@@ -0,0 +1,14 @@
+package com.spryrocks.imgsearch.presentation.ui.screens;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.spryrocks.imgsearch.presentation.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
