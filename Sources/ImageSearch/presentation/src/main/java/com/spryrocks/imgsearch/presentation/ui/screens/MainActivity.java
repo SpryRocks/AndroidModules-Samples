7c1afe46676263f9d261bd592ba05259d8877b41
maxim <maxim.zhemerenko@spryrocks.com>
2017-05-16 17:32:40 +0300

    Add Android Annotations

--- Sources/ImageSearch/presentation/src/main/java/com/spryrocks/imgsearch/presentation/ui/screens/MainActivity.java
+++ Sources/ImageSearch/presentation/src/main/java/com/spryrocks/imgsearch/presentation/ui/screens/MainActivity.java
@@ -1,14 +1,13 @@
 package com.spryrocks.imgsearch.presentation.ui.screens;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 import com.spryrocks.imgsearch.presentation.R;
 
+import org.androidannotations.annotations.EActivity;
+
+@SuppressLint("Registered")
+@EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 }
