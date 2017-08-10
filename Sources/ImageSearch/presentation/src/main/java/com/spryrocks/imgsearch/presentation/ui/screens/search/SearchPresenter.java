b3cd762eedbac230746541b79f8e5fec0d634143
Roman Dyukachov <roman.dyukachov@spryrocks.com>
2017-08-10 12:28:05 +0300

    Attach to lifecycle

--- Sources/ImageSearch/presentation/src/main/java/com/spryrocks/imgsearch/presentation/ui/screens/search/SearchPresenter.java
+++ Sources/ImageSearch/presentation/src/main/java/com/spryrocks/imgsearch/presentation/ui/screens/search/SearchPresenter.java
@@ -4,23 +4,34 @@ import com.spryrocks.android.modules.ui.mvp.v1.ui.PresenterBase;
 import com.spryrocks.imgsearch.domain.interactors.ISearchInteractor;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.schedulers.Schedulers;
 
 class SearchPresenter extends PresenterBase<ISearchView> implements ISearchPresenter {
     private final ISearchInteractor searchInteractor; // TODO: 18.05.2017 use manager, handle lifecycle, cancel preview requests
+    private final CompositeDisposable compositeDisposable;
 
     SearchPresenter(ISearchInteractor searchInteractor) {
         this.searchInteractor = searchInteractor;
+        this.compositeDisposable = new CompositeDisposable();
     }
 
     @Override
     public void search() {
         String searchQuery = getView().getSearchQuery();
 
-        searchInteractor.searchImages(searchQuery) // TODO: 19.05.2017 attach to lifecycle
+        searchInteractor.searchImages(searchQuery)
+                .doOnSubscribe(compositeDisposable::add)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(getView()::showImages,
                         getView()::handleError);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        compositeDisposable.clear();
     }
 }
