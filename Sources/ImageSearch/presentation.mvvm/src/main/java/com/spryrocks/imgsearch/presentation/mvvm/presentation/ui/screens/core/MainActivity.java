91f9f5afe84d3b4a67845e8319b8cbb41a028fc2
Roman Dyukachov <roman.dyukachov@spryrocks.com>
2017-08-23 12:09:22 +0300

    Create XML-file Create model Attach Data to View

--- Sources/ImageSearch/presentation.mvvm/src/main/java/com/spryrocks/imgsearch/presentation/mvvm/presentation/ui/screens/core/MainActivity.java
+++ Sources/ImageSearch/presentation.mvvm/src/main/java/com/spryrocks/imgsearch/presentation/mvvm/presentation/ui/screens/core/MainActivity.java
@@ -1,22 +1,21 @@
 package com.spryrocks.imgsearch.presentation.mvvm.presentation.ui.screens.core;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.presentationmvvm.R;
-
+import com.example.presentationmvvm.databinding.FragmentSearchBinding;
+import com.spryrocks.imgsearch.presentation.mvvm.presentation.ui.screens.core.model.Image;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        SearchFragment searchFragment = SearchFragment_.builder().build();
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.content_view, searchFragment)
-                .commit();
+        FragmentSearchBinding binding = DataBindingUtil.setContentView(this, R.layout.fragment_search);
+        Image image = new Image("Binding working!");
+        binding.setImages(image);
     }
 }
---
+++ Sources/ImageSearch/presentation.mvvm/src/main/java/com/spryrocks/imgsearch/presentation/mvvm/presentation/ui/screens/core/model/Image.java
@@ -0,0 +1,9 @@
+package com.spryrocks.imgsearch.presentation.mvvm.presentation.ui.screens.core.model;
+
+public class Image {
+    public final String title;
+
+    public Image(String title) {
+        this.title = title;
+    }
+}
